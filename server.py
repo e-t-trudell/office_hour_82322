a43cf02dacda4b314ff88b90384acb615b28cd42
Eric <ettrudell17@gmail.com>
2022-08-23 20:24:19 -0600

    second push

--- server.py
+++ server.py
@@ -7,11 +7,33 @@ def index():
 
 @app.route('/run')
 def run():
-    return render_template('index.html')
+    return render_template('run.html')
 
 @app.route('/pet')
 def pet():
-    return render_template('index.html')
+    return render_template('pet.html')
+
+@app.route('/bite')
+def bite():
+    return render_template('bite.html')
+
+@app.route('/climb')
+def climb():
+    return render_template('climb.html')
+
+@app.route('/cower')
+def cower():
+    return render_template('cower.html')
+
+@app.route('/friend')
+def friend():
+    return render_template('friend.html')
+
+@app.route('/losthand')
+def losthand():
+    return render_template('losthand.html')
+
+
 
 if __name__=="__main__":   
     app.run(debug=True)      
